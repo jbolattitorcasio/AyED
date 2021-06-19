d631042d58ba37eebbee6cbf09fdf804b3bab694
jbolattitorcasio <83435787+jbolattitorcasio@users.noreply.github.com>
2021-06-19 12:50:38 -0300

    Update main.cpp

--- TP 1/Ejercicio 4/main.cpp
+++ TP 1/Ejercicio 4/main.cpp
@@ -8,9 +8,10 @@ int main()
     float a, b, c, x1, x2;
     cout << "Ingrese los valores a, b y c de una ecuacion cuadratica: " << endl;
     cin >> a >> b >> c;
-    x1= ((-b+sqrt(pow(b, 2)-4*a*c))/2*a);
-    x2= ((-b-sqrt(pow(b, 2)-4*a*c))/2*a);
-    if ((pow(b, 2)-4*a*c) >= 0) {
+
+    if (((pow(b, 2)-4*a*c) >= 0) && (2*a != 0)) {
+        x1= ((-b+sqrt(pow(b, 2)-4*a*c))/2*a);
+        x2= ((-b-sqrt(pow(b, 2)-4*a*c))/2*a);
         cout << "Los ceros de la funcion (raices) son : " << x1 << " y " << x2 << endl;
     } else {
         cout << "No hay raices posibles" << endl;
