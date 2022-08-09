2320f368f2028af777d8eb39c8840a9be2f26134
JuanC <83435787+jbolattitorcasio@users.noreply.github.com>
2022-08-08 23:09:00 -0300

    Create 01.cpp

---
+++ Estudio/Ejercicios Resueltos/01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout << "Ingrese a:" << endl;
+    cin >> a;
+    cout << "Ingrese b:" << endl;
+    cin >> b;
+    cout << "a+b:" << a+b << endl;
+    cout << "a-b:" << a-b << endl;
+    cout << "a*b:" << a*b << endl;
+    return 0;
+}
